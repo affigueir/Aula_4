9210214ac070c881e4c6c0d5b081ee393fc2b6c7
affigueir <affigueir@gmail.com>
2022-06-30 17:59:26 +0100

    Add count to N Method

--- MyFirstMethod/Program.cs
+++ MyFirstMethod/Program.cs
@@ -6,13 +6,30 @@ namespace MyFirstMethod
     {
         static void Main(string[] args)
         {
-            CountTo10();
-            CountTo10();
+            //CountTo10();
+            //CountTo10();
+            System.Console.Write("Type a number: ");
+            int i = Int32.Parse(Console.ReadLine());
+            
+            System.Console.Write("Type another number: ");
+            int j = Int32.Parse(Console.ReadLine());
+
+            CountToN(i);
+            CountToN(j);
+            
         }
 
         private static void CountTo10()
         {
             for(int i = 1; i <= 10; i++)
+            {
+                System.Console.WriteLine(i);
+            }
+        }
+
+        private static void CountToN(int n)
+        {
+            for(int i = 1; i <= n; i++)
             {
                 System.Console.WriteLine(i);
             }
